73d9f784f06b143fe55d8b5993e7f4fea61d13d6
Yu Jin Kim <yujinkim@fb.com>
2019-07-08 15:26:02 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by yujinkim on 7/8/19.
+//  Copyright © 2019 yujinkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by yujinkim on 7/8/19.
+//  Copyright © 2019 yujinkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
